0f8bfeb71c69573574d8f0113d390ae51b227f4b
Jessi Vargas <jekavargas.cr@gmail.com>
2026-03-01 20:33:22 -0600

    Add files via upload

---
+++ Semana 3/EstudianteBecado.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PooPracticaClase3
+{
+    public class EstudianteBecado : Estudiante
+    {
+        public double MontoBeca { get; set; }
+
+        public EstudianteBecado(string carrera, double montoBeca) : base(carrera)
+        {
+            this.MontoBeca = montoBeca;
+        }
+
+        public override void MostrarInformacion()
+        {
+            Console.WriteLine($"Estudiante becado: {GetNombre()}, Edad: {Edad}, Carrera: {MontoBeca}");
+        }
+    }
+}
